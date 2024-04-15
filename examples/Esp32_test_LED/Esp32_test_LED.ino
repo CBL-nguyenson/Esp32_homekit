1beeb94bc217fbcc8da82de5e852687d05c765a5
CBL-nguyenson <cbl.nguyenson@gmail.com>
2024-04-15 10:43:34 +0700

    hehe

--- examples/Esp32_test_LED/Esp32_test_LED.ino
+++ examples/Esp32_test_LED/Esp32_test_LED.ino
@@ -1,10 +1,14 @@
+#include <Arduino.h>
 void setup()
 {
   // put your setup code here, to run once:
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
 }
 
 void loop()
 {
   // put your main code here, to run re
-  
 }
